abd9ddcf13984d625e89db56a871d01274a8f343
Markuss9 <player1.markuss@gmail.com>
2026-06-04 23:58:56 +0200

    fix treesitter parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,27 +5,6 @@ return {
   main = 'nvim-treesitter',
   opts = {
     auto_install = true,
+    highlight = { enable = true },
   },
-
-  init = function()
-    local ensureInstalled = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-
-    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
-    local parsersToInstall = vim
-      .iter(ensureInstalled)
-      :filter(function(parser)
-        return not vim.tbl_contains(alreadyInstalled, parser)
-      end)
-      :totable()
-    require('nvim-treesitter').install(parsersToInstall)
-
-    vim.api.nvim_create_autocmd('FileType', {
-      callback = function()
-        -- Enable treesitter highlighting and disable regex syntax
-        pcall(vim.treesitter.start)
-        -- Enable treesitter-based indentation
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
 }
